26bbb4b1070f78d3b1c19d0edcb778295884adbc
root <zwdbox@zwdbox.club>
2018-10-23 08:28:47 +0800

    v1

---
+++ test2/ReadAllText/ReadAllText/Program.cs
@@ -0,0 +1,20 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadAllText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EWordDocument eWordDocument = new EWordDocument();
+            eWordDocument.Open(@"D:\教学\2018-2019\农业研究生C#\CSharp\test2\科研细则.docx");
+            Console.WriteLine(eWordDocument.Text);
+        }
+    }
+}
